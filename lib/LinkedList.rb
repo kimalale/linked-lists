83347b49a56961cf259a4f28a1053c8d707713a6
Kim Malale <kimalale@hotmail.com>
2023-05-10 15:34:24 +0200

    Completed implementing functions

--- lib/LinkedList.rb
+++ lib/LinkedList.rb
@@ -2,13 +2,13 @@
 class LinkedList
 
   def initialize(data = nil)
-    node = Node.new(data)
+    @node = Node.new(data)
   end
 
 #append(value) adds a new node containing value to the end of the list
   def append(value)
     append_node = Node.new(value)
-    current_node = node
+    current_node = @node
     until current_node.next_node == nil do
       current_node = current_node.next_node
     end
@@ -18,16 +18,14 @@ class LinkedList
 #prepend(value) adds a new node containing value to the start of the list
   def prepend(value)
 
-    new_node = Node.new(value, node)
-
-    node = new_node
-
+    new_node = Node.new(value, @node)
+    @node = new_node
   end
 
 #size returns the total number of nodes in the list
   def size
    count = 1
-   current_node = node
+   current_node = @node
    until current_node.next_node == nil do
     count +=1
     current_node = current_node.next_node
@@ -37,21 +35,84 @@ class LinkedList
 
 #head returns the first node in the list
   def head
-    node.data
+    @node.data
   end
 
 #tail returns the last node in the list
   def tail
 
-    last_node = node
-    # self.size.times do
-    #   last_node = last_node.new_node
-    # end
+    last_node = @node
+    self.size.times do
+      last_node = last_node.next_node
+    end
 
     last_node.data
   end
 
 #at(index) returns the node at the given index
+  def at(index)
+
+    current_node = @node
+    index.times do
+      current_node = current_node.next_node
+    end
+    current_node.data
+  end
+
+#pop removes the last element from the list
+  def pop
+    last_node = @node
+    self.size.times do
+      last_node = last_node.next_node
+    end
+
+    popped_node = last_node.data
+    last_node = nil
+    popped_node
+  end
+
+#contains?(value) returns true if the passed in value is in the list and otherwise returns false.
+  def contains?(value)
+    in_list = false
+    search_node = @node
+    self.size.times do
+      in_list = true if search_node.data == value
+      search_node = search_node.next_node
+    end
+
+    in_list
+  end
+
+#find(value) returns the index of the node containing value, or nil if not found.
+  def find(value)
+    index = 0
+
+    search_node = @node
+    self.size.times do
+
+      return index if search_node.data == value
+      search_node = search_node.next_node
+      index+=1
+    end
+
+    "Index not found."
+  end
+
+
+  #to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+  def to_s
+
+    print_node = @node
+    self.size.times  do
+      print "#{print_node.data} "
+      print_node = print_node.next_node
+    end
+  end
+
+
+# Extra Credit
+#insert_at(value, index) that inserts a new node with the provided value at the given index.
+
 end
 
 
@@ -64,7 +125,15 @@ class Node
 
 end
 
+test_list = LinkedList.new(2)
+test_list.append(3)
+test_list.prepend(1)
+test_list.append(4)
+test_list.prepend(0)
+test_list.append(5)
 
+
+test_list.to_s
 
 
 
@@ -74,12 +143,9 @@ end
 
 
 
-#at(index) returns the node at the given index
-#pop removes the last element from the list
-#contains?(value) returns true if the passed in value is in the list and otherwise returns false.
-#find(value) returns the index of the node containing value, or nil if not found.
-#to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
-# Extra Credit
-#insert_at(value, index) that inserts a new node with the provided value at the given index.
+
+
+
+
 #remove_at(index) that removes the node at the given index.
 # Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their #next_node link updated.
